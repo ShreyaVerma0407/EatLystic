4b965f141ab04bce6003566a9679ea1853dea9d5
khushi <145266963+khushi519g5@users.noreply.github.com>
2025-08-30 22:09:55 +0530

    updated routes

--- Backend/index.js
+++ Backend/index.js
@@ -10,15 +10,12 @@ import consumedRoutes from "./server/routes/consumed.js"; // <-- new route
 import totalNutrientRoutes from "./server/routes/nutrientsTotal.js";
 import bodyParser from "body-parser";
 import fitnessRoutes from "./server/routes/fitnessRoutes.js";
-<<<<<<< HEAD
+
  import notificationRoutes from "./server/routes/notifications.js";
  import emailRoutes from "./server/routes/emailController.js";
  import pdfEmailRoute from "./server/routes/pdfEmailRoute.js";
-=======
-// import notificationRoutes from "./server/routes/notifications.js";
-// import emailRoutes from "./server/routes/emailController.js";
-// import pdfEmailRoute from "./server/routes/pdfEmailRoute.js";
->>>>>>> 7bb196977496a50eb314289ee5a150929ecd9472
+
+
 
 const app = express();
 app.use(express.json());
@@ -93,11 +90,8 @@ app.use("/api/fitness", fitnessRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/email", emailRoutes);
 app.use("/api/email", pdfEmailRoute);
-=======
-// app.use("/api/notifications", notificationRoutes);
-// app.use("/email", emailRoutes);
-// app.use("/api/email", pdfEmailRoute);
->>>>>>> 7bb196977496a50eb314289ee5a150929ecd9472
+
+
 
 // --------------------
 // Start Server
