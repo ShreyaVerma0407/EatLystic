5ae4cf305d5ff50acbb0f0d7e7963046971dd149
khushi <145266963+khushi519g5@users.noreply.github.com>
2025-08-30 22:05:17 +0530

    WIP: local changes before pulling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,14 @@ import Pantry from './Components/Pantry';
 import CaloriePage from './Components/CaloriePage';
 import NutrientPage from './Components/NutrientPage'; // Import Calorie page component
 import FitnessTrackerPremium from "./Components/FitnessTracker";
+import PantryReport from "./Components/PantryReport";
+import Recipe from './Components/Recipe.jsx';
+import PantryChef from './Components/PantryChef.jsx';
+import Try from './Components/Try.jsx';
+import MacrosChef from './Components/MacrosChef.jsx';
+// import Liked from './Components/Liked.jsx';
+import Customise from './Components/Customise.jsx';
+import Dishes from './Components/Dishes.jsx';
 import { useState, useEffect } from 'react';
 
 function App() {
@@ -31,10 +39,19 @@ function App() {
         <Route path="/home" element={<Homepage />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/pantry" element={<Pantry currentUserId={currentUserId} />} />
-        <Route path="/calorie" element={<CaloriePage currentUserId={currentUserId} />} /> 
+        <Route path="/calorie" element={<CaloriePage currentUserId={currentUserId} />} />
         {/* Calorie route */}
         <Route path="/nutrient" element={<NutrientPage />} />
         <Route path="/fitness" element={<FitnessTrackerPremium />} />
+        <Route path="/recipe" element={<Recipe/>}/>
+        <Route path="/recipe/pantrychef" element={<PantryChef/>}/>
+        <Route path="/try" element={<Try/>}/>
+        <Route path ="recipe/pantrychef/dishes" element={<Dishes/>}/>
+        <Route path="/recipe/macroschef" element ={<MacrosChef/>}/>
+         {/* <Route path="/recipe/like" element ={<Liked/>}/> */}
+        <Route path="/recipe/customise" element ={<Customise/>}/>
+        <Route path="/pantryreport" element={<PantryReport userId={currentUserId} />} />
+
       </Routes>
     </BrowserRouter>
   );
