77a6f15c0ff1441976e4979e0429ba880d846475
Shreya Verma <vshreya0407@gmail.com>
2025-09-19 22:27:07 +0530

    updated routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signup from './signup'; 
+import Signup from './signup';
 import Login from './login';
 import Welcome from './Components/Welcome';
 import Homepage from './Components/Homepage';
@@ -14,14 +14,15 @@ import Recipe from './Components/Recipe.jsx';
 import PantryChef from './Components/PantryChef.jsx';
 import Try from './Components/Try.jsx';
 import MacrosChef from './Components/MacrosChef.jsx';
-import Liked from './Components/Liked';  {/* Liked component imported */}
+import Liked from './Components/Liked';
 import Customise from './Components/Customise.jsx';
 import Dishes from './Components/Dishes.jsx';
 import FoodList from './Components/FoodList.jsx';
 import { useState, useEffect } from 'react';
-import ErrorBoundary from './Components/ErrorBoundary';  {/* ErrorBoundary imported */}
+import ErrorBoundary from './Components/ErrorBoundary';
 import NotFound from "./Components/NotFound";
 import ShoppingCart from "./Components/shopping.jsx";
+
 function App() {
   const [currentUserId, setCurrentUserId] = useState(null);
 
@@ -41,7 +42,7 @@ function App() {
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/" element={<LandingPage />} />
-        
+
         {/* Routes that require userId */}
         <Route path="/pantry" element={<Pantry currentUserId={currentUserId} />} />
         <Route path="/calorie" element={<CaloriePage currentUserId={currentUserId} />} />
@@ -54,21 +55,21 @@ function App() {
         <Route path="/recipe/macroschef" element={<MacrosChef />} />
         <Route path="*" element={<NotFound />} />
         {/* Liked Recipes Route wrapped in ErrorBoundary */}
-        <Route 
-          path="/recipe/liked" 
+        <Route
+          path="/recipe/liked"
           element={
             <ErrorBoundary>
               <Liked userId={currentUserId} />
             </ErrorBoundary>
-          } 
+          }
         />
-        
-        {/* Customisation Route */}
-        <Route path="/recipe/customise" element={<Customise />} />
-        
+
+        {/* Customisation Route with userId prop */}
+        <Route path="/recipe/customise" element={<Customise userId={currentUserId} />} />
+
         {/* Pantry Report Route */}
         <Route path="/pantryreport" element={<PantryReport userId={currentUserId} />} />
-        
+
         {/* Food List Route */}
         <Route path="/food" element={<FoodList />} />
         <Route path="/ShoppingCart" element={<ShoppingCart />} />
