c0659619fa3335421dd4fd1fa4ff22dca6fd8a7d
Shreya Verma <vshreya0407@gmail.com>
2025-09-20 20:58:27 +0530

    added routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -27,6 +27,7 @@ import Dishes_meal from './Components/Dishes_meal.jsx';
 import HelpDesk from './Components/HelpDesk.jsx';
 import ContactUs from './Components/ContactUs.jsx';
 import Feedback from './Components/Feedback.jsx';
+import Faq from './Components/Faq.jsx';
 
 function App() {
   const [currentUserId, setCurrentUserId] = useState(null);
@@ -61,6 +62,7 @@ function App() {
         <Route path="/helpdesk" element={<HelpDesk/>}/>
         <Route path="/helpdesk/contactus" element={<ContactUs/>}/>
         <Route path="/helpdesk/feedback" element={<Feedback/>}/>
+        <Route path="/helpdesk/faq" element={<Faq/>}/>
         <Route path="*" element={<NotFound />} />
         {/* Liked Recipes Route wrapped in ErrorBoundary */}
         <Route
