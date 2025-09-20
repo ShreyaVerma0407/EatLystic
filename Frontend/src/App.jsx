6955239f04c4f03b23dcadbb2f55d85e9af38b6b
Shreya Verma <vshreya0407@gmail.com>
2025-09-20 16:07:35 +0530

    routes updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -26,6 +26,7 @@ import MealChef from './Components/MealChef.jsx';
 import Dishes_meal from './Components/Dishes_meal.jsx';
 import HelpDesk from './Components/HelpDesk.jsx';
 import ContactUs from './Components/ContactUs.jsx';
+import Feedback from './Components/Feedback.jsx';
 
 function App() {
   const [currentUserId, setCurrentUserId] = useState(null);
@@ -59,6 +60,7 @@ function App() {
         <Route path="/recipe/macroschef" element={<MacrosChef />} />
         <Route path="/helpdesk" element={<HelpDesk/>}/>
         <Route path="/helpdesk/contactus" element={<ContactUs/>}/>
+        <Route path="/helpdesk/feedback" element={<Feedback/>}/>
         <Route path="*" element={<NotFound />} />
         {/* Liked Recipes Route wrapped in ErrorBoundary */}
         <Route
