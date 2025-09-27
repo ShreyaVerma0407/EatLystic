99e1125ec996df92e41f3ddd3fb6cd40752f1351
Shreya Verma <vshreya0407@gmail.com>
2025-09-28 03:20:33 +0530

    liked

--- Frontend/src/Components/Liked.jsx
+++ Frontend/src/Components/Liked.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Components/Navbar"; 
 import "../styles/Liked.css";
-
+import Footer from './Footer';
 const Liked = ({ userId }) => {
   const [likedRecipes, setLikedRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -125,7 +125,7 @@ const Liked = ({ userId }) => {
             ))}
           </div>
         </div>
-      </div>
+      </div><Footer/>
     </div>
   );
 };
