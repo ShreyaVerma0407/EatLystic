1fc6c453a52d19566607458b1f9779ea06b5d120
Shreya Verma <vshreya0407@gmail.com>
2026-06-30 19:51:30 +0530

    removed unwanted files

--- camera.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from eatlystic_bill_parser import streamlit_bill_uploader
-
-st.set_page_config(page_title="Eatlystic", page_icon="🛒")
-st.title("🛒 Eatlystic — Grocery Bill Scanner")
-streamlit_bill_uploader()
